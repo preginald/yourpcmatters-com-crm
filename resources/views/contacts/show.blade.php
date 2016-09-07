ac4b681cc0abfc7362c7ae8889f523da31bb174b
preginald <peter@yourpcmatters.com>
2016-09-07 20:53:35 +1000

    created contact show view

---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', '- Show New Contact')
+
+@section('content')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Show New Contact</div>
+
+            <div class="panel-body">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

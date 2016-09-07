2400dff41b6be9903d5a7ff5022cd617d1ba209f
preginald <peter@yourpcmatters.com>
2016-09-07 16:08:14 +1000

    created contact create form view

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', '- Create New Contact')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create New Contact</div>
+
+                <div class="panel-body">
+                    {!! Form::open(['route' => 'contacts.store']) !!}
+                        <div class="form-group">
+                            {{ Form::label('firstname', 'First Name') }}
+                            {{ Form::text('firstname', null, ['class' => 'form-control']) }}
+                        </div>
+                        <div class="form-group">
+                            {{ Form::label('lastname', 'Last Name') }}
+                            {{ Form::text('lastname', null, ['class' => 'form-control']) }}
+                        </div>
+                        <div class="form-group">
+                            {{ Form::label('email', 'Email') }}
+                            {{ Form::text('email', null, ['class' => 'form-control']) }}
+                        </div>                        
+                        {{ Form::submit('Create Contact', ['class' => 'btn btn-success btn-block']) }} 
+                   {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

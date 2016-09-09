4308619def092d80e279486302e1d05c9c7c58d0
preginald <peter@yourpcmatters.com>
2016-09-09 10:54:45 +1000

    fixed form model binding, removed cancel and save buttons from side bar and moved them to form panel

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -9,7 +9,7 @@
             <div class="panel-heading">Edit Contact</div>
                 
                 <div class="panel-body">
-                    {!! Form::model($contact, ['route' => ['contacts.update', $contact->id]]) !!}
+                    {!! Form::model($contact, ['method' => 'PATCH', 'route' => ['contacts.update', $contact->id]]) !!}
                     <div class="form-group">
                         {{ Form::label('firstname', 'First Name') }}
                         {{ Form::text('firstname', null, ['class' => 'form-control']) }}
@@ -22,6 +22,10 @@
                         {{ Form::label('email', 'Email') }}
                         {{ Form::text('email', null, ['class' => 'form-control']) }}
                     </div>                        
+                    <div class="form-group">
+                        {!! Html::linkRoute('contacts.show', 'Cancel', [$contact->id], ['class' => "btn btn-default"]) !!}
+                        {{ Form::submit('Save Changes', ['class' => 'btn btn-success']) }}
+                    </div>
                     {!! Form::close() !!}
                 </div>
 
@@ -30,28 +34,17 @@
         </div>
         
         <div class="col-md-4">
-
-        <div class="well">
-            <dl class="dl-horizontal">
-                <dt>Created:</dt>
-                <dd>{{ $contact->created_at }}</dd>
-            </dl>
-            
-            <dl class="dl-horizontal">
-                <dt>Updated:</dt>
-                <dd>{{ $contact->updated_at }}</dd>
-            </dl>
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created:</dt>
+                    <dd>{{ $contact->created_at }}</dd>
+                </dl>
+                
+                <dl class="dl-horizontal">
+                    <dt>Updated:</dt>
+                    <dd>{{ $contact->updated_at }}</dd>
+                </dl>
         
-            <hr>
-            
-            <div class="row">
-                <div class="col-sm-6">
-                    {!! Html::linkRoute('contacts.show', 'Cancel', [$contact->id], ['class' => "btn btn-warning btn-block"]) !!}
-                </div> 
-
-                <div class="col-sm-6">
-                    {!! Html::linkRoute('contacts.update', 'Save', [$contact->id], ['class' => "btn btn-success btn-block"]) !!}
-                </div> 
 
 </div>
 @endsection

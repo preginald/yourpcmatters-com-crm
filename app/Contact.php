f1e9a581ef686bb90a408476db5aaf9504923c0a
preginald <peter@yourpcmatters.com>
2016-09-13 12:08:14 +1000

    added softdeletes

--- app/Contact.php
+++ app/Contact.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    
     public function accounts()
     {
         return $this->belongsToMany('App\Account');

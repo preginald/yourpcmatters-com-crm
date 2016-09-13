dd58142a900510d026d80bb343c8c32892508b7b
preginald <peter@yourpcmatters.com>
2016-09-13 11:37:08 +1000

    removed right side panel and moved timestamps to main panel footer and moved edit button to panel heading and delete to panel footer. changed panel heading to h4.

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -6,62 +6,56 @@
 <div class="row">
     <div class="col-md-8">
         <div class="panel panel-default">
-            <div class="panel-heading">Basic Details</div>
-                
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <dl class="dl">
-                                <dt>Name:</dt>
-                                <dd>{{ $contact->firstname }} {{ $contact->lastname }}</dd>
-                            </dl>
-                        </div>
-
-                        <div class="col-sm-4">
-                            <dl class="dl">
-                                <dt>Email:</dt>
-                                <dd>{{ $contact->email }}</dd>
-                            </dl>
-                        </div>
-
-                        <div class="col-sm-4">
-                            <dl class="dl">
-                                <dt>Phone:</dt>
-                                <dd>{{ $contact->phone }}</dd>
-                            </dl>
-                        </div>
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h4>Basic Details</h4>
+                    </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('contacts.edit', 'Edit', [$contact->id], ['class' => "btn btn-default pull-right"]) !!}
                     </div>
                 </div>
-
             </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <dl class="dl">
+                            <dt>Name:</dt>
+                            <dd>{{ $contact->firstname }} {{ $contact->lastname }}</dd>
+                        </dl>
+                    </div>
 
-        </div>
-        
-        <div class="col-md-4">
+                    <div class="col-sm-4">
+                        <dl class="dl">
+                            <dt>Email:</dt>
+                            <dd>{{ $contact->email }}</dd>
+                        </dl>
+                    </div>
 
-        <div class="well">
-            <dl class="dl-horizontal">
-                <dt>Created:</dt>
-                <dd>{{ $contact->created_at }}</dd>
-            </dl>
-            
-            <dl class="dl-horizontal">
-                <dt>Updated:</dt>
-                <dd>{{ $contact->updated_at }}</dd>
-            </dl>
-        
-            <hr>
-            
-            <div class="row">
-                <div class="col-sm-6">
-                    {!! Html::linkRoute('contacts.edit', 'Edit', [$contact->id], ['class' => "btn btn-primary btn-block"]) !!}
-                </div> 
-
-                <div class="col-sm-6">
-                    {!! Form::open(['route' => ['contacts.destroy', $contact->id],'method' => 'DELETE']) !!}
-                    {!! Form::submit('Delete', ['class' => "btn btn-danger btn-block"]) !!}
-                    {!! Form::close() !!}
-                </div> 
-
-</div>
+                    <div class="col-sm-4">
+                        <dl class="dl">
+                            <dt>Phone:</dt>
+                            <dd>{{ $contact->phone }}</dd>
+                        </dl>
+                    </div>
+                </div><!-- end of row -->
+            </div><!-- end of panel-body --> 
+            <div class="panel-footer">
+                <div class="row">
+                    <div class="col-sm-5">
+                        Created: {{ $contact->created_at }}
+                    </div>
+                    <div class="col-sm-5">
+                        Updated: {{ $contact->updated_at }}
+                    </div>
+                    <div class="col-sm-2">
+                        {!! Form::open(['route' => ['contacts.destroy', $contact->id],'method' => 'DELETE']) !!}
+                        {!! Form::submit('Delete', ['class' => "btn btn-link btn-xs pull-right"]) !!}
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div><!-- end of panel --> 
+    </div><!-- end of col-md-8 --> 
+</div><!-- end of content row -->
 @endsection

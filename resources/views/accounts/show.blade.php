3d35228a02b69cef84a4fe9189e607d4dc71b6dc
preginald <peter@yourpcmatters.com>
2016-09-17 15:27:34 +1000

    removed side bar and moved timestamps to panel footer and edit button to panel header

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -6,62 +6,57 @@
 <div class="row">
     <div class="col-md-8">
         <div class="panel panel-default">
-            <div class="panel-heading">Basic Details</div>
-                
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <dl class="dl">
-                                <dt>Name:</dt>
-                                <dd>{{ $account->name }}</dd>
-                            </dl>
-                        </div>
-
-                        <div class="col-sm-4">
-                            <dl class="dl">
-                                <dt>Email:</dt>
-                                <dd>{{ $account->email }}</dd>
-                            </dl>
-                        </div>
-
-                        <div class="col-sm-4">
-                            <dl class="dl">
-                                <dt>Phone:</dt>
-                                <dd>{{ $account->phone }}</dd>
-                            </dl>
-                        </div>
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h4>Account Details</h4>
                     </div>
-                </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('accounts.edit', 'Edit', [$account->id], ['class' => "btn btn-default pull-right"]) !!}
+                    </div>
+                </div><!-- end of row -->
+            </div><!-- end of panel-heading -->
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <dl class="dl">
+                            <dt>Name:</dt
+                                <dd>{{ $account->name }}</dd>
+                        </dl>
+                    </div>
 
-            </div>
+                    <div class="col-sm-4">
+                        <dl class="dl">
+                            <dt>Email:</dt>
+                            <dd>{{ $account->email }}</dd>
+                        </dl>
+                    </div>
 
-        </div>
-        
-        <div class="col-md-4">
-
-        <div class="well">
-            <dl class="dl-horizontal">
-                <dt>Created:</dt>
-                <dd>{{ $account->created_at }}</dd>
-            </dl>
-            
-            <dl class="dl-horizontal">
-                <dt>Updated:</dt>
-                <dd>{{ $account->updated_at }}</dd>
-            </dl>
-        
-            <hr>
-            
-            <div class="row">
-                <div class="col-sm-6">
-                    {!! Html::linkRoute('accounts.edit', 'Edit', [$account->id], ['class' => "btn btn-primary btn-block"]) !!}
-                </div> 
-
-                <div class="col-sm-6">
+                    <div class="col-sm-4">
+                        <dl class="dl">
+                            <dt>Phone:</dt>
+                            <dd>{{ $account->phone }}</dd>
+                        </dl>
+                    </div>
+                </div><!-- end of row -->
+            </div><!-- end of panel-body -->
+            <div class="panel-footer">
+                <div class="row">
+                    <div class="col-sm-5">
+                        Created: {{ $account->created_at }}
+                    </div>
+                    <div class="col-sm-5">
+                        Updated: {{ $account->updated_at }}
+                    </div>
+                    <div class="col-sm-2">
                     {!! Form::open(['route' => ['accounts.destroy', $account->id],'method' => 'DELETE']) !!}
-                    {!! Form::submit('Delete', ['class' => "btn btn-danger btn-block"]) !!}
+                    {!! Form::submit('Delete', ['class' => "btn btn-link btn-xs pull-right"]) !!}
                     {!! Form::close() !!}
-                </div> 
-
-</div>
+                    </div>
+                </div><!-- end of row -->
+            </div><!-- end of panel-footer -->
+        </div><!-- end of panel -->
+        @include('accounts.partials.contacts');
+    </div><!-- end of col-md-8 -->
+</div><!-- end of row -->
 @endsection

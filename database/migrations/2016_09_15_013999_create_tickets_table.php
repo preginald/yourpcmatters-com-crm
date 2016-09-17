e29281a829c7f50e8d9d0d7048675e9a3d7c2933
preginald <peter@yourpcmatters.com>
2016-09-17 15:29:28 +1000

    updated migration fields

--- database/migrations/2016_09_15_013999_create_tickets_table.php
+++ database/migrations/2016_09_15_013999_create_tickets_table.php
@@ -15,11 +15,15 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('account_id')->unsigned();
+            $table->integer('contact_id')->unsigned();
             $table->integer('type_id')->unsigned();
             $table->integer('priority_id')->unsigned();
             $table->integer('status_id')->unsigned();
             $table->string('subject');
             $table->text('discription');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreign('contact_id')->references('id')->on('contacts');
             $table->foreign('type_id')->references('id')->on('ticket_types');
             $table->foreign('priority_id')->references('id')->on('ticket_priorities');
             $table->foreign('status_id')->references('id')->on('ticket_statuses');

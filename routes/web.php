91ffb43fbf79f9e6cf5931f1fa5811ce6fd1a84b
preginald <peter@yourpcmatters.com>
2016-09-19 16:11:10 +1000

    added route for ticket creation

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,15 @@ Route::resource('/leads', 'LeadController');
 
 Route::resource('/accounts', 'AccountController');
 
+Route::resource('/contacts', 'ContactController');
+
+Route::resource('/tickets', 'TicketController');
+
+// Account Contact Routes
 Route::get('accounts/{account}/search/contacts', 'AccountContactController@showSearch')->name('accounts.searchcontacts');
 Route::get('accounts/{account}/contacts/{contact}', 'AccountContactController@show')->name('accounts.contacts.show');
 Route::post('/accounts/search/contacts', 'AccountContactController@search')->name('accounts.search.contacts');
 Route::post('/accounts/attach/contacts', 'AccountContactController@attach')->name('accounts.attach.contacts');
 
-Route::resource('/contacts', 'ContactController');
-
-Route::resource('/tickets', 'TicketController');
+// Account Ticket Routes
+Route::get('accounts/{account}/contacts/{contact}/tickets/create', 'AccountTicketController@create')->name('accounts.contacts.tickets.create');

99c4d43fbfe52a98f8693860251b96b43e7b6d48
preginald <peter@yourpcmatters.com>
2016-09-19 16:07:13 +1000

    added relationships

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function account()
+    {
+        return $this->belongsTo('App\Account');
+    }
+
+    public function contact()
+    {
+        return $this->belongsTo('App\Contact');
+    }
+    
+    public function status()
+    {
+        return $this->belongsTo('App\TicketStatus');
+    }
+    
+    public function priority()
+    {
+        return $this->belongsTo('App\TicketPriority');
+    }
+    
+    public function type()
+    {
+        return $this->belongsTo('App\TicketType');
+    }
 }

4248dbcaa8ce0ef594cecf46162ec3eb9aaa3fff
preginald <peter@yourpcmatters.com>
2016-09-20 20:12:01 +1000

    added diffForHumans date attribute

--- app/Ticket.php
+++ app/Ticket.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon;
 
 class Ticket extends Model
 {
@@ -34,5 +35,11 @@ class Ticket extends Model
     public function comments()
     {
         return $this->morphMany('App\Comment', 'commentable');
+    }
+    
+    public function getUpdatedAtDiffAttribute()
+    {
+        return Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['updated_at'])->diffForHumans();
+        
     }
 }

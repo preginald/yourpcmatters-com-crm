9df10bac534da409ba5a08aa3d27483ab7a73147
preginald <peter@yourpcmatters.com>
2016-09-20 13:12:43 +1000

    added comments index view to ticket show view

---
+++ resources/views/comments/index.blade.php
@@ -0,0 +1,24 @@
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Comments
+        </div>
+        <div class="panel-body">
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <header>
+                        <ul class="list-inline">
+                            <li><span class="label label-info">preginald</span></li>
+                            <li><small>Sat 28-Feb 11:58 AM</small></li>
+                        </ul>
+                    </header>
+                    <body>Dundee Utd - Patrick Thistle @ 2.05 (68% favorites) submitted by /u/LSSB</body>
+                    <footer>
+                        <ul class="list-inline">
+                            <li><a href="#"><small>reply</small></a></li>
+                            <li><a href="#"><small>edit</small></a></li>             
+                        </ul>
+                    </footer>
+                </li>
+            </ul>
+        </div>
+    </div>
